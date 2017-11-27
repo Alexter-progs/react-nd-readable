77ae52fb222309f1c7c5d03d0f0aae70d0a73ed3
Alexter-progs <alexplotbox@gmail.com>
2017-11-27 20:17:33 +0300

    remove initial markup

--- frontend/src/Root/App.js
+++ frontend/src/Root/App.js
@@ -6,22 +6,6 @@ import Typography from 'material-ui/Typography';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      backend: 'backend-data'
-    }
-  }
-
-  componentDidMount() {
-    const url = `${process.env.REACT_APP_BACKEND}/categories`;
-    fetch(url)
-      .then((res) => { return(res.text()) })
-      .then((data) => {
-        this.setState({backend:data});
-      });
-  }
-
   render() {
     return (
       <div className="App">
@@ -32,17 +16,6 @@ class App extends Component {
             </Typography>
           </Toolbar>
         </Header>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Talking to the backend yields these categories: <br/>
-          {this.state.backend}
-        </p>
       </div>
     );
   }
