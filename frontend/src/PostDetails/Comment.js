a8d9a6291322422c8879cc2e007de11056445069
Alexter-progs <alexplotbox@gmail.com>
2017-12-03 21:43:02 +0300

    add action buttons for comment

--- frontend/src/PostDetails/Comment.js
+++ frontend/src/PostDetails/Comment.js
@@ -6,6 +6,10 @@ import { formatDate } from '../utils'
 import IconButton from 'material-ui/IconButton'
 import RemoveCircleOutline from 'material-ui-icons/RemoveCircleOutline'
 import AddCircleOutline from 'material-ui-icons/AddCircleOutline'
+import ModeEditIcon from 'material-ui-icons/ModeEdit'
+import DeleteIcon from 'material-ui-icons/Delete'
+import Button from 'material-ui/Button'
+import Grid from 'material-ui/Grid'
 
 export default class Comment extends Component {
 
@@ -15,12 +19,27 @@ export default class Comment extends Component {
         return (
             <Card>
                 <CardContent>
-                    <Typography style={{fontWeight: 'bold'}}>
-                    {author}<Typography noWrap> at {formatDate(timestamp)}</Typography>
-                    </Typography>
+                    <Grid container alignItems='flex-end'>
+                        <Grid item xs={3} lg={3} md={3}>
+                            <Typography style={{fontWeight: 'bold'}}>
+                            {author}<Typography noWrap> at {formatDate(timestamp)}</Typography>
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={7} lg={7} md={7}/>
+                        <Grid item xs={1} lg={1} md={1}>
+                            <Button fab color="primary" aria-label="add" style={{width: '50px', height: '50px'}}>
+                                <ModeEditIcon />
+                            </Button>
+                        </Grid>
+                        <Grid item xs={1} lg={1} md={1}>
+                            <Button fab color="primary" aria-label="add" style={{width: '50px', height: '50px'}}>
+                                <DeleteIcon />
+                            </Button>
+                        </Grid>
+                    </Grid>
+                    
                     <Divider/>
                     <Typography>{body}</Typography>
-                    
                 </CardContent>
                 <CardActions>
                     <IconButton  color="accent">
@@ -31,7 +50,7 @@ export default class Comment extends Component {
                     </Typography>
                     <IconButton  color="primary">
                           <AddCircleOutline />
-                        </IconButton>
+                    </IconButton>
                 </CardActions>
             </Card>
         )
